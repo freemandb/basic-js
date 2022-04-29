b9797ec98f2eb14e271bca0094db7cf20c2294bf
denis.borushko <denis.borushko@vizor-games.com>
2022-04-29 23:43:50 +0300

    carbon and count cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let a = parseFloat(sampleActivity)
+  if (typeof sampleActivity != 'string' || isNaN(a) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  
+  return Math.ceil( Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
 }
+
+// console.log(dateSample(3.142)); 
+
 
 module.exports = {
   dateSample
