34fee53934a1be935736a006b79b0bc77b9de2c5
denis.borushko <denis.borushko@vizor-games.com>
2022-05-02 00:04:12 +0300

    encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str === '') {
+    return '';
+  }
+  let strArr = [...str];
+  let curChar = strArr[0];
+  let count = 0;
+  let resStr = '';
+  strArr.forEach(char => {
+    if(curChar === char) {
+      count++;
+    }
+    else {
+      resStr += (count > 1 ? count : '') + curChar;
+      count = 1;
+      curChar = char;
+    }
+  })
+  if (count > 0) {
+    resStr += (count > 1 ? count : '') + curChar;
+  }
+  return resStr;
 }
+
+// encodeLine('aaaatttt')
 
 module.exports = {
   encodeLine
